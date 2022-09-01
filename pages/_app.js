893dad86b1d0e2be0a47f300c3f1bf775765baad
robertwhite22 <robertwhite.dev@gmail.com>
2022-09-01 18:41:12 -0400

    test dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { SessionProvider } from 'next-auth/react'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 export default function App({
@@ -6,6 +7,9 @@ export default function App({
   router,
   pageProps: { session, ...pageProps },
 }) {
+  useEffect(() => {
+    console.log(document)
+  }, [])
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
 import { useSession, signIn, signOut } from 'next-auth/react'
+import { Fragment, useEffect } from 'react'
 
 export default function Home() {
   const { data: session } = useSession()
-
+  useEffect(() => {}, [])
   if (session) {
     return (
-      <>
+      <main id='home-page' className='home-page-container'>
         Signed in as {session?.token?.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
-      </>
+      </main>
     )
   } else {
     return (
-      <>
+      <main id='home-page' className='home-page-container'>
         Not signed in <br />
         <button onClick={() => signIn()}>Sign in</button>
-      </>
+        <button>Toggle Color Mode</button>
+      </main>
     )
   }
 }
